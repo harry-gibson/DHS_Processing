50f8d8e87aca3027e230ff82377c174733b6d2df
harry-gibson <harry.s.gibson@gmail.com>
2016-01-06 17:35:09 +0000

    Added FME workbenches for building quality extraction
    DHS surveys extraction for PWG and Lucy Tusting - These extractions were
    coded using FME. They are notable for the use of schema mappings to
    optionally replace numeric coded values with their textual meaning
    (which varies between surveys) and also optionally rename or copy the
    variables themselves to reflect the text of the question that was asked.

---
+++ HouseholdElectricity/ExtractHouseholdElectrificationQuery.sql
@@ -0,0 +1,7 @@
+﻿SELECT dhs_data_tables."RECH0".surveyid, dhs_data_tables."RECH0".HHID, dhs_data_tables."RECH0".HV000, dhs_data_tables."RECH0".HV001, dhs_data_tables."RECH0".HV002, dhs_data_tables."RECH0".HV005, dhs_data_tables."RECH0".HV008, dhs_data_tables."RECH0".HV009, dhs_data_tables."RECH0".HV024, dhs_data_tables."RECH0".HV025, dhs_data_tables."RECH2".HV201, dhs_data_tables."RECH2".HV206, dhs_data_tables."RECH2".HV207, dhs_data_tables."RECH2".HV208, dhs_data_tables."RECH2".HV209, dhs_data_tables."RECH2".HV210, dhs_data_tables."RECH2".HV211, dhs_data_tables."RECH2".HV212, dhs_data_tables."RECH2".HV221, dhs_data_tables."RECH2".HV270, dhs_data_tables."RECH2".HV271 
+from dhs_data_tables."RECH0" LEFT OUTER JOIN dhs_data_tables."RECH2" 
+ON 
+dhs_data_tables."RECH0".HHID = dhs_data_tables."RECH2".HHID
+and 
+dhs_data_tables."RECH0".surveyid = dhs_data_tables."RECH2".surveyid
+where dhs_data_tables."RECH0".surveyid != 156
